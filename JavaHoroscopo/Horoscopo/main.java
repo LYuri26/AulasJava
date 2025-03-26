53774cb37df1f8abe6a70cf2c0c14de08927b56b
Lenon Yuri <lenonyurisilva26@gmail.com>
2025-03-26 14:45:00 -0300

    26/03/25

--- JavaHoroscopo/Horoscopo/main.java
+++ JavaHoroscopo/Horoscopo/main.java
@@ -1,40 +1,37 @@
 import java.util.Scanner;
 
 public class main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita ao usuário para inserir a data de nascimento
         System.out.print("Digite a data no formato dd/mm/aaaa: ");
-        String data = scanner.nextLine();
+        String dataNascimento = scanner.nextLine();
 
-        // Solicita ao usuário para inserir a hora de nascimento
         System.out.print("Digite a hora de nascimento (formato 24h - hh:mm): ");
         String horaNascimento = scanner.nextLine();
 
-        // Solicita ao usuário para inserir a hora do nascer do sol
         System.out.print("Digite a hora do nascer do sol (formato 24h - hh:mm): ");
-        String horaNascerDoSol = scanner.nextLine();
+        String horaNascerSol = scanner.nextLine();
 
-        // Cria uma instância (objeto) da classe 'signo' para determinar o signo solar
-        signo s = new signo();
+        // Instância para cálculo do signo solar
+        signo calculadoraSigno = new signo();
 
-        // Cria uma instância (objeto) da classe 'ascendente' para determinar o signo ascendente
-        ascendente a = new ascendente();
+        // Instância para cálculo do signo ascendente
+        ascendente calculadoraAscendente = new ascendente();
 
-        // Determina o signo solar usando o objeto 's' da classe 'signo'
-        String signoSolar = s.determinarsigno(data);
+        // Calcula o signo solar
+        String signoSolar = calculadoraSigno.determinarsigno(dataNascimento);
 
-        // Determina o signo ascendente usando o objeto 'a' da classe 'ascendente'
-        String signoascendente = a.determinarascendente(data, horaNascimento, horaNascerDoSol);
+        // Calcula o signo ascendente
+        String signoAscendente = calculadoraAscendente.determinarascendente(
+                dataNascimento, horaNascimento, horaNascerSol);
 
-        // Verifica se os dados inseridos são válidos e imprime os resultados
-        if (!signoSolar.equals("dados inválidos") && !signoascendente.equals("dados inválidos")) {
-            System.out.println("O signo solar é: " + signoSolar);
-            System.out.println("O signo ascendente é: " + signoascendente);
+        if (!signoSolar.equals("Dados inválidos") && !signoAscendente.equals("Dados inválidos")) {
+            System.out.println("\nResultados:");
+            System.out.println("Signo Solar: " + signoSolar);
+            System.out.println("Signo Ascendente: " + signoAscendente);
         } else {
-            System.out.println("Dados inválidos");
+            System.out.println("Dados inválidos fornecidos. Verifique as informações inseridas.");
         }
 
         scanner.close();
